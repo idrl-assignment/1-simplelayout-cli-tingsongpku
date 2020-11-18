53b054a6e6f2b07c3b4141d645dfdd63d7523747
jason <tingsong@pku.edu.cn>
2020-11-19 00:21:21 +0800

    argparse参数中含有空格必须把参数加“”当字符串处理？
    命令行解析

--- simplelayout.py
+++ simplelayout.py
@@ -7,20 +7,22 @@ def main():
     parser.add_argument("-b", "--board_grid", type=int, help="board_length")
     parser.add_argument("-u", "--unit_grid", type=int, help="unit_length")
     parser.add_argument("--unit_n", type=int, help="unit numbers")
-    parser.add_argument("--positions", type=int)
+    parser.add_argument("--positions", type=str)
     parser.add_argument("-o", "--outdir", type=str, default="example_dir")
     parser.add_argument("--file_name", type=str, default="example")
     args = parser.parse_args()
     if args.board_grid % args.unit_grid != 0:
         print("mod error")
         exit()
-    if args.positions != args.unit_n:
-        print("#positions != #unit_n, error")
+    positions = args.positions.split(' ')  # --positions 1 4
+    if len(positions) != args.unit_n:
         exit()
-    if args.positions >= int(args.board_grid/args.unit_grid) ^ 2:
-        print("too large, error")
-        exit()
-    #if args.outdir not in os.listdir("."):无法遍历深层目录，os.listdir只能读取一层，读取深层需要结合os.walk
+    else:
+        for pos in positions:
+            if int(pos) >= int(args.board_grid/args.unit_grid) ** 2:
+                print("too large, error")
+                print(pos, int(args.board_grid/args.unit_grid) ** 2)
+                exit()
     if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
     f = open('./%s/%s.mat' % (args.outdir, args.file_name), 'w')
