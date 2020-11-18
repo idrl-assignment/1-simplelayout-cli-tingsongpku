76e94b1738d1ca901eee8390682def7d84d0e24e
jason <tingsong@pku.edu.cn>
2020-11-19 00:54:19 +0800

    某一位置参数含空格问题
    nargs='+'

--- simplelayout.py
+++ simplelayout.py
@@ -7,20 +7,20 @@ def main():
     parser.add_argument("-b", "--board_grid", type=int, help="board_length")
     parser.add_argument("-u", "--unit_grid", type=int, help="unit_length")
     parser.add_argument("--unit_n", type=int, help="unit numbers")
-    parser.add_argument("--positions", type=str)
+    parser.add_argument("--positions", nargs='+', type=int)  # nargs='+'
     parser.add_argument("-o", "--outdir", type=str, default="example_dir")
     parser.add_argument("--file_name", type=str, default="example")
     args = parser.parse_args()
     if args.board_grid % args.unit_grid != 0:
         print("mod error")
         exit()
-    positions = args.positions[:-1].split(' ')  # --positions 1 4
-    print(positions)
-    if len(positions) != args.unit_n:
+    #  positions = args.positions[:-1].split(' ')  # --positions 1 4
+    print(args.positions)
+    if len(args.positions) != args.unit_n:
         print("grid numbers error")
         exit()
     else:
-        for pos in positions:
+        for pos in args.positions:
             if int(pos) >= int(args.board_grid/args.unit_grid) ** 2:
                 print("too large, error")
                 print(pos, int(args.board_grid/args.unit_grid) ** 2)
